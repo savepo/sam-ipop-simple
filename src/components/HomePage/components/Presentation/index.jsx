b1c1b967f52b0439772527660f6e26fcba19a39f
savepo <samverstegen95@gmail.com>
2023-05-27 00:36:11 +0200

    test

--- src/components/HomePage/components/Presentation/index.jsx
+++ src/components/HomePage/components/Presentation/index.jsx
@@ -5,7 +5,7 @@ import { homePageText } from '../../../../pageData/personalIntroduction';
 const Presentation = () => {
   return (
     <PresentationContainer>
-      <Title>Presentación</Title>
+      <Title>Presentción</Title>
       <Text>{homePageText.introduction}</Text>
     </PresentationContainer>
   );
